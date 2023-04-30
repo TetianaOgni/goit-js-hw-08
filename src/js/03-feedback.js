fab4de62f0ad5d137a3466313131c1d38c76f644
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-04-30 19:54:16 +0300

    made 03task universal

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,29 +2,27 @@ import throttle from 'lodash.throttle';
 import { save, load, remove } from './storage';
 
 const formEl = document.querySelector('.feedback-form');
-const emailInput = document.querySelector('input[type=email]');
-const messageInput = document.querySelector('textarea');
+// const emailInput = document.querySelector('input[type=email]');
+// const messageInput = document.querySelector('textarea');
 const STORAGE_KEY = 'feedback-form-state';
 
 formEl.addEventListener('input', throttle(onInputSave, 500));
 window.addEventListener('DOMContentLoaded', fillFields);
 formEl.addEventListener('submit', onSubmit);
 
-function onInputSave() {
-  const formData = {
-    email: emailInput.value.trim(),
-    message: messageInput.value.trim(),
-  };
+let formData = {};
+
+function onInputSave(event) {
+  formData[event.target.name] = event.target.value;
   save(STORAGE_KEY, formData);
 }
 
 function onSubmit(event) {
   event.preventDefault();
-  console.log(load(STORAGE_KEY));
+  console.log(formData);
   remove(STORAGE_KEY);
-  emailInput.value = '';
-  messageInput.value = '';
-  console.log(load(STORAGE_KEY));
+  formEl.reset();
+  formData = {};
 }
 
 function fillFields() {
@@ -32,6 +30,9 @@ function fillFields() {
   if (!savedInput) {
     return;
   }
-  emailInput.value = savedInput.email;
-  messageInput.value = savedInput.message;
+
+  formData = savedInput;
+  Object.entries(formData).forEach(([key, value]) => {
+    formEl.elements[key].value = value;
+  });
 }
